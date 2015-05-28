d5b34ca38145607bb1370966d575bcd6efac408e
milenapribic <pribicmilena@gmail.com>
2015-05-28 16:27:52 -0400

    changed blog section

--- about.php
+++ about.php
@@ -1,5 +1,17 @@
-<main>
-	<div class="hello">
-		<h1>What is PHP?</h1>
-	</div>
-</main>
+<?php
+
+/*Template Name: About*/
+get_header();  ?>
+
+
+<div class="main">
+  <div class="container">
+
+      <h2><?php the_title(); ?></h2>
+      <?php the_content(); ?>
+
+    <?php endwhile; // end the loop?>
+  </div> <!-- /.container -->
+</div> <!-- /.main -->
+
+<?php get_footer(); ?>
