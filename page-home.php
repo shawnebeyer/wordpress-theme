b3e2efb3182bc1da7484900e66a205a47df7ff15
Sebeyer <shawnebeyer@yahoo.com>
2015-05-31 01:22:54 -0400

    changes to header.php page-home.php home, sidebar and style.scss.

--- page-home.php
+++ page-home.php
@@ -14,9 +14,10 @@ get_header();  ?>
         <?php the_content(); ?>
 
       <?php endwhile; // end the loop?>
+
     </div> <!-- /,content -->
       <body>
-        <main>
+        <main class="mainHome">
           <div class="slideshow">
             <img src="http://41.media.tumblr.com/ccc2511b5cf920591b1fa00ce19c6e41/tumblr_nod41bzpqj1st5lhmo4_1280.jpg" alt="">
             <img src="http://40.media.tumblr.com/893ecde6ca0d92213d09b2dc1000bf37/tumblr_no832q33ZY1st5lhmo3_1280.jpg" alt="">
